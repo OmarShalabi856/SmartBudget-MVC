6f7a47ec7c9bd28604749bca0f9476e9a03718f3
Omar Shalabi <omarshalabi856@gmail.com>
2024-03-29 23:33:57 +0200

    Tested The First Part Of The Added Models With The Generated Controllers and Updated The Styling

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -6,12 +6,10 @@ namespace SmartBudget.Models
 	{
 		[Key]
 		public int Id { get; set; }
-
-		public int Amount { get; set; }
+        public int Amount { get; set; }
 		public string? Note { get; set; }
 		public DateTime Date { get; set; } = DateTime.Now;
-
 		public int CategoryId { get; set; }
-		public Category Category { get; set; }
+		public Category? Category { get; set; }
 	}
 }
